f42d140984d79591b9d7d0bf344b4d459717eb07
tanyinghui <yhtan927@gmail.com>
2019-03-11 11:42:04 +0800

    add keybinding func

--- client/components/01_Main/bottombar/ControlPanel.js
+++ client/components/01_Main/bottombar/ControlPanel.js
@@ -4,6 +4,72 @@ import Reverse from '../../../../public/reverse.png';
 import Stop from '../../../../public/stop.png';
 
 class ControlPanel extends React.Component {
+
+    constructor(props) {
+        super(props);
+        this.handleKeyPress = this.handleKeyPress.bind(this);
+        this.reverse = this.reverse.bind(this);
+        this.stop = this.stop.bind(this);
+        this.forward = this.forward.bind(this);
+        this.BW = this.BW.bind(this);
+        this.CC = this.CC.bind(this);
+        this.HP = this.HP.bind(this);
+    };
+    
+    componentDidMount() {
+        document.addEventListener('keydown', this.handleKeyPress);
+    };
+
+    componentWillUnmount() {
+        document.removeEventListener('keydown', this.handleKeyPress);
+    };
+
+    reverse() {
+        alert('REVERSE')
+    };
+
+    stop() {
+        alert('STOP')
+    };
+
+    forward() {
+        alert('FORWARD')
+    };
+
+    BW() {
+        alert('BW')
+    };
+
+    CC() {
+        alert('CC')
+    };
+
+    HP() {
+        alert('HP')
+    };
+
+    handleKeyPress(e) {
+        if (e.keyCode === 81) {
+            // Q for reverse
+            this.reverse();
+        } else if (e.keyCode === 82) {
+            // R for stop
+            this.stop();
+        } else if (e.keyCode === 83) {
+            // S for forward
+            this.forward();
+        } else if (e.keyCode === 78) {
+            // N for BW
+            this.BW();
+        } else if (e.keyCode === 75) {
+            // K for CC
+            this.CC();
+        } else if (e.keyCode === 80) {
+            // P for HP 
+            this.HP();
+        }
+    }
+
     render() {
         const controlkey = {
             display: 'flex',
@@ -22,18 +88,18 @@ class ControlPanel extends React.Component {
 
         return(
             <div style={controlkey}>
-                <button>
+                <button onClick={this.reverse}>
                     <img src={Reverse} style={imgbtn} />
                 </button>
-                <button>
+                <button onClick={this.stop}>
                     <img src={Stop} style={imgbtn} />
                 </button>
-                <button>
+                <button onClick={this.forward}>
                     <img src={Forward} style={imgbtn} />
                 </button>
-                <button style={btn}>CC</button>
-                <button style={btn}>HP</button>
-                <button style={btn}>BW</button>
+                <button onClick={this.CC} style={btn}>CC</button>
+                <button onClick={this.HP} style={btn}>HP</button>
+                <button onClick={this.BW} style={btn}>BW</button>
             </div>
         )
     }
