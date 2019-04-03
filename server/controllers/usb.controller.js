f762dcbf8d6485ca87c5248b9ce12978ad874b6e
yhtan927@gmail.com <yhtan927@gmail.com>
2019-04-03 14:46:03 +0800

    accept merge

--- server/controllers/usb.controller.js
+++ server/controllers/usb.controller.js
@@ -1,14 +1,3 @@
-<<<<<<< HEAD
-import HttpStatus from 'http-status-codes';
-
-export function usbcontrol(req, res) {
-    const { data } = req.body;
-    console.log('The data is ' + data)
-    res.json({
-        success: true
-    })
-};
-=======
 export function usbcontrol(req, res) {
     const { data } = req.body;
     console.log('DATA: ' + data);
@@ -16,4 +5,3 @@ export function usbcontrol(req, res) {
         success: true
     });
 };
->>>>>>> 68f2e68a42bc68b34d9224a96d1ca1aab7b07465
