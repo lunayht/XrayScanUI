a67ffcbfde2f11d82ce414c5c7c65491f57eca37
yhtan927@gmail.com <yhtan927@gmail.com>
2019-04-09 16:10:57 +0800

    cleanup routes

--- client/routers/MainRouter.js
+++ client/routers/MainRouter.js
@@ -3,14 +3,12 @@ import { Route, Switch } from 'react-router-dom';
 import LoginPage from '../components/00_Login/LoginPage';
 import MainPage from '../components/01_Main/MainPage';
 import Authentication from './Authentication';
-import AlertDialog from '../components/01_Main/alert/AlertDialog';
 
 const MainRouter = () => (
     <Fragment>
         <Switch>
             <Route exact path="/" component={LoginPage} />
             <Authentication path="/main" component={MainPage} />
-            <Route path="/test" component={AlertDialog} />
         </Switch>
     </Fragment>
 );
